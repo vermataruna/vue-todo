bb68a92174c50f0d3224bd2d10afbc28d2c0fdfc
vermataruna <tarunaverma792@gmail.com>
2023-09-18 21:53:07 +0530

    add: todos view and create todos component

---
+++ src/views/TodosView.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <h1>Create Todo</h1>
+    <TodoCreator />
+  </main>
+</template>
+
+<script setup>
+import TodoCreator from '../components/TodoCreator.vue';
+
+</script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
