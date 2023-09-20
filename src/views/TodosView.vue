cd8e8bf1eff2d7abdab43d4c71eeec63ba1ea805
vermataruna <tarunaverma792@gmail.com>
2023-09-20 23:43:32 +0530

    add: todos view style

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -2,11 +2,15 @@
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo"/>
+    <ul class="todo-list">
+      <TodoItem v-for="todo in todoList" :todo="todo"></TodoItem>
+    </ul>
   </main>
 </template>
 
 <script setup>
 import TodoCreator from '../components/TodoCreator.vue';
+import TodoItem from '../components/TodoItem.vue';
 import { ref } from 'vue';
 import { uid } from 'uid';
 
@@ -35,6 +39,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
